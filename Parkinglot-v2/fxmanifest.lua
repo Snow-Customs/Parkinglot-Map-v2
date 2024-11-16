a5ce44abb0e4b9024217c4919291ef9bb83ae88f
Snow-Custom <153662202+Snow-Custom@users.noreply.github.com>
2024-11-17 00:34:08 +0100

    Uploaded Files

---
+++ Parkinglot-v2/fxmanifest.lua
@@ -0,0 +1,11 @@
+fx_version 'cerulean'
+game 'gta5'
+author 'Snow-Customs'
+description 'Parkinglot Meetingpoint v2.0'
+version '0.0.2'
+this_is_a_map 'yes'
+
+dependencies { 
+    '/server:5503',
+    '/gameBuild:2545',
+}
